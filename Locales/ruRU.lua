6c5d1120e96087124b5096a4a90970cee447f3b1
Sacred Duckwhale <duckwhale@mail.ru>
2017-08-25 06:29:27 +0200

    Added translated phrases from CurseForge

---
+++ Locales/ruRU.lua
@@ -0,0 +1,93 @@
+L = L or {}
+L["BarBuff"] = "Иконки баффов"
+L["BarDebuff"] = "Иконки дебаффов"
+L["BarHidden"] = "Скрыт"
+L["BarName"] = "Вид панели иконок"
+--Translation missing 
+L["BarProg"] = "Progs"
+--Translation missing 
+L["BarSecure"] = "Secure"
+L["BarWeapon"] = "Иконки оружия"
+L["BlizzDesc"] = "Показать фрейм баффов Blizzard "
+L["BlizzName"] = "Blizzard "
+--Translation missing 
+L["CancelAura"] = [=[
+Bison Cancel Aura : "%s"
+
+Blizzard changed the call of CancelUnitBuff. Now these function can only be used by secure environment with SecureButtons. The handling of secure buttons is very restricted. I use this workaround for the moment. 
+
+Copy this line and paste it to the chat.]=]
+L["ColsDesc"] = "Количество колонок"
+L["ColsName"] = "Колонки"
+L["CooldownDesc"] = "Показать анимацию восстановления."
+L["CooldownName"] = "Восстановление"
+L["DebugDesc"] = "Отображение отладочной информации. Требуется только для тестирования."
+L["DebugName"] = "Отладка"
+--Translation missing 
+L["DescBuff"] = "Helpful buffs."
+--Translation missing 
+L["DescDebuff"] = "Harmful buffs."
+--Translation missing 
+L["DescHidden"] = "Joined helpful buffs at the tooltip. (need enabled option on buffs)"
+--Translation missing 
+L["DescProgs"] = "Special helpful buffs."
+L["Description"] = "Позволяет свободно размещать и отображать все пользовательские баффы, дебаффы и зачарования оружия."
+--Translation missing 
+L["DescWeapon"] = "Weapon enchant buffs."
+L["EnabledDesc"] = "Активирует аддон и скрывает стандартный фрейм (де)баффов."
+L["EnabledName"] = "Включить"
+L["FlashingDesc"] = "Мигание при заканчивании времени."
+L["FlashingName"] = "Включить мигание"
+--Translation missing 
+L["GapDesc"] = "Gap between icon and time"
+--Translation missing 
+L["GapName"] = "Gap"
+--Translation missing 
+L["Help"] = "Help"
+L["HorizontalDesc"] = "Первое направление для отображения иконок."
+L["HorizontalName"] = "Первое по горизонтали"
+--Translation missing 
+L["JoinDesc"] = "Join some special buffs."
+--Translation missing 
+L["JoinName"] = "Join Buffs"
+L["LockDesc"] = "Фиксирует все иконки после определения их позиций."
+L["LockName"] = "Фиксировать иконки"
+--Translation missing 
+L["NumberDesc"] = "Maximal visible number of buttons."
+--Translation missing 
+L["NumberName"] = "Size"
+L["OptionName"] = "Общие настройки"
+L["Profile"] = "Профиль"
+L["ProfileAvailable"] = "Доступные профиля"
+L["ProfileCopied"] = "Копирование настроек с \"%s\""
+L["ProfileCreated"] = "Создание нового профиля \"%s\""
+L["ProfileDeleted"] = "Удаление профиля \"%s\""
+L["ProfileLoaded"] = "Установка профиля к \"%s\""
+L["ProfileReset"] = "Сброс профиля \"%s\""
+L["RowsDesc"] = "Количество рядов"
+L["RowsName"] = "Ряды"
+L["ScaleDesc"] = "Размер иконок."
+L["ScaleName"] = "Масштаб"
+L["ShowDesc"] = "Отображение/сокрытие иконок."
+L["ShowName"] = "Панель иконок"
+L["SortAlpha"] = "Название"
+L["SortDec"] = "Остаток, Убывание"
+L["SortDesc"] = "Установка сортировки иконок баффов"
+L["SortDurationAsc"] = "Длительность"
+L["SortDurationDesc"] = "Длительность, Убывание"
+L["SortInc"] = "Остаток"
+L["SortName"] = "Сортировка"
+L["SortNone"] = "Нету (Blizzard order)"
+L["SortRevert"] = "Название, Убывание"
+--Translation missing 
+L["SpellDesc"] = "The spell ID"
+--Translation missing 
+L["SpellName"] = "SpellID"
+L["TimerDesc"] = "Изменение стиля время между Bisonовским или Blizzardским."
+L["TimerName"] = "Отображение времени"
+L["Version"] = "Версия"
+L["VersionDesc"] = "Выводит информации о версии."
+L["XPaddingDesc"] = "Промежуток по горизонтали между иконками (мин: -20, макс: 20). Отрицательное значение изменит направление панели."
+L["XPaddingName"] = "Промежуток - Горизонталь"
+L["YPaddingDesc"] = "Промежуток по вертикали между иконками (мин: -50, макс: 50). Отрицательное значение изменит направление панели."
+L["YPaddingName"] = "Промежуток - Вертикаль"
